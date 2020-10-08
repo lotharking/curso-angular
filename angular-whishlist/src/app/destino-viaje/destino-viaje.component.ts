8835659be2244f1837bad00eb53141e527917659
Andres Vallecilla <andresfelipe083195@gmail.com>
2020-10-08 17:18:42 -0500

    Se creo un formulario con angular

---
+++ angular-whishlist/src/app/destino-viaje/destino-viaje.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { DestinoViaje } from './../models/destino-viaje.model';
+
+@Component({
+  selector: 'app-destino-viaje',
+  templateUrl: './destino-viaje.component.html',
+  styleUrls: ['./destino-viaje.component.css']
+})
+export class DestinoViajeComponent implements OnInit {
+	@Input() destino: DestinoViaje;
+	@HostBinding('attr.class') cssClass = "col-md-4";//para modificar la clase que engloba todo
+
+  constructor() { 
+  	
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular-whishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { DestinoViaje } from './../models/destino-viaje.model';
+
+@Component({
+  selector: 'app-lista-destinos',
+  templateUrl: './lista-destinos.component.html',
+  styleUrls: ['./lista-destinos.component.css']
+})
+export class ListaDestinosComponent implements OnInit {
+  destinos: string[];
+  constructor() {
+  	this.destinos = [];
+   }
+
+  ngOnInit(): void {
+  }
+ guardar(nombre, url): boolean{
+ 	this.destinos.push(new DestinoViaje(nombre, url));
+ 	return false;
+ }
+}
